00223675181782c5744d6a5d435987a5889306e5
Human <54731850+Ramiels@users.noreply.github.com>
2024-09-09 02:56:32 -0400

    g

--- Rainbow/Items/Tier2/CloakOnKill.cs
+++ Rainbow/Items/Tier2/CloakOnKill.cs
@@ -8,27 +8,17 @@ namespace Rainbow.Items
 {
 	public class CloakOnKill : ItemBase
 	{
-		public override string ItemName				=> "EXAMPLE_CLOAKONKILL_NAME";
+		public override string ItemName				=> "ITEM_CLOAKONKILL_NAME";
 		public override string ItemLangTokenName	=> "CLOAKONKILL";
-		public override string ItemPickupDesc		=> "EXAMPLE_CLOAKONKILL_PICKUP";
-		public override string ItemFullDescription	=> "EXAMPLE_CLOAKONKILL_DESC";
-		public override string ItemLore				=> "EXAMPLE_CLOAKONKILL_LORE";
+		public override string ItemPickupDesc		=> "ITEM_CLOAKONKILL_PICKUP";
+		public override string ItemFullDescription	=> "ITEM_CLOAKONKILL_DESC";
+		public override string ItemLore				=> "ITEM_CLOAKONKILL_LORE";
 		public override ItemTier Tier				=> ItemTier.Tier2;
 		public override GameObject ItemModel		=> Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
 		public override Sprite ItemIcon				=> Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
 		public override ItemDisplayRuleDict CreateItemDisplayRules() {
 			// One day...
 			var rules = new ItemDisplayRuleDict();
-			rules.Add("mdlCommandoDualies", new RoR2.ItemDisplayRule[] {
-				new RoR2.ItemDisplayRule {
-					ruleType = ItemDisplayRuleType.ParentedPrefab,
-					followerPrefab = ItemModel,
-					childName = "Chest",
-					localPos = new Vector3(0F, 0.17296F, 0.20893F),
-					localAngles = new Vector3(80.00002F, 180F, 180F),
-					localScale = new Vector3(0.08412F, 0.06451F, 0.06451F)
-				}
-			});
 			return rules;
 		}
 
