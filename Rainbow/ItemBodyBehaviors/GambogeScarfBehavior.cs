1a97888d3cc2b694b7d29acacba06917acb31f34
Human <54731850+Ramiels@users.noreply.github.com>
2024-09-11 05:38:17 -0400

    not working

---
+++ Rainbow/ItemBodyBehaviors/GambogeScarfBehavior.cs
@@ -0,0 +1,61 @@
+﻿using RoR2;
+using RoR2.Items;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UnityEngine;
+
+namespace Rainbow.ItemBodyBehaviors
+{
+	internal class GambogeScarfBehavior : BaseItemBodyBehavior
+	{
+		[BaseItemBodyBehavior.ItemDefAssociationAttribute(useOnServer = true, useOnClient = false)]
+		private static ItemDef GetItemDef()
+		{
+			return Rainbow.Items.GambogeScarf;
+		}
+
+		private bool providingBuff;
+
+		// Handle in/out of combat behavior
+		private void OnDisable()
+		{
+			this.SetProvidingBuff(false);
+		}
+		private void FixedUpdate()
+		{
+			this.SetProvidingBuff(base.body.outOfCombat);
+		}
+
+		private void SetProvidingBuff(bool shouldProvideBuff)
+		{
+			if (shouldProvideBuff == this.providingBuff)
+			{
+				return;
+			}
+			this.providingBuff = shouldProvideBuff;
+			if (this.providingBuff)
+			{
+				base.body.AddBuff(RoR2Content.Buffs.WhipBoost);
+				EffectData effectData = new EffectData();
+				effectData.origin = base.body.corePosition;
+				CharacterDirection characterDirection = base.body.characterDirection;
+				bool flag = false;
+				if (characterDirection && characterDirection.moveVector != Vector3.zero)
+				{
+					effectData.rotation = Util.QuaternionSafeLookRotation(characterDirection.moveVector);
+					flag = true;
+				}
+				if (!flag)
+				{
+					effectData.rotation = base.body.transform.rotation;
+				}
+				EffectManager.SpawnEffect(LegacyResourcesAPI.Load<GameObject>("Prefabs/Effects/SprintActivate"), effectData, true);
+				return;
+			}
+			base.body.RemoveBuff(RoR2Content.Buffs.WhipBoost);
+		}
+
+	}
+}
---
+++ Rainbow/Items/Tier2/GambogeScarf.cs
@@ -0,0 +1,53 @@
+﻿using R2API;
+using RoR2;
+using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Rainbow.Items
+{
+	public class GambogeScarf : ItemBase
+	{
+		public override string ItemLangTokenName	=> "GambogeScarf";
+		public override string ItemName				=> "ITEM_GambogeScarf_NAME";
+		public override string ItemPickupDesc		=> "ITEM_GambogeScarf_PICKUP";
+		public override string ItemFullDescription	=> "ITEM_GambogeScarf_DESC";
+		public override string ItemLore				=> "ITEM_GambogeScarf_LORE";
+		public override ItemTier Tier				=> ItemTier.Tier2;
+		public override ItemTag[] ItemTags			=> [];
+		public override GameObject ItemModel		=> Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
+		public override Sprite ItemIcon				=> Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
+		public override ItemDisplayRuleDict CreateItemDisplayRules() {
+			// One day...
+			var rules = new ItemDisplayRuleDict();
+			return rules;
+		}
+
+		// Hook shit
+		public override void Hooks() {
+			GlobalEventManager.onCharacterDeathGlobal += (damageReport) => {
+				DamageReport report = damageReport;
+
+				// If a character was killed by the world, we shouldn't do anything.
+				if (!report.attacker || !report.attackerBody) { return; }
+
+				var attackerCharacterBody = report.attackerBody;
+
+				// We need an inventory to do check for our item
+				if (attackerCharacterBody.inventory) {
+					var garbCount = GetCount(attackerCharacterBody);
+					if (garbCount > 0 && Util.CheckRoll(50, attackerCharacterBody.master)) {
+						attackerCharacterBody.AddTimedBuff(RoR2Content.Buffs.Cloak, 3 + garbCount);
+					}
+				}
+			};
+		}
+
+		// Initialize the item
+		public override void Init() {
+			CreateLang();
+			CreateItem();
+			Hooks();
+		}
+	}
+}
